837c3221ff59b1aa1b8db58e8c9bf2dbde6e40b1
PawWitGit <git.pawelwitala@gmail.com>
2022-11-29 18:16:42 +0100

    first_steps

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/items_not_int/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
