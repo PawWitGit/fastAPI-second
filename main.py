cc3714acf008995a3c4283a4d887b166d51a92b7
PawWitGit <git.pawelwitala@gmail.com>
2022-11-29 20:32:42 +0100

    query parameters

--- main.py
+++ main.py
@@ -3,15 +3,87 @@ from enum import Enum
 
 app = FastAPI()
 
+# ******** -------- ********
+"""
+Query parameters
+"""
+app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
 
 # ******** -------- ********
 
+
+# ******** -------- ********
+
+
+# @app.get("/items/{item_id}")
+# async def read_user_item(item_id: str, needy: str):
+#     item = {"item_id": item_id, "needy": needy}
+#     return item
+
+
+# ******** -------- ********
+
+
+# @app.get("/users/{user_id}/item/{item_id}")
+# async def read_user_item(
+#     user_id: int, item_id: str, q: str | None = None, short: bool = False
+# ):
+#     item = {"item_id": item_id, "owner_id": user_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update({"description": "This is an amazing item has a long decsription"})
+#     return item
+
+
+# ******** -------- ********
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None, short: bool = False):
+#     item = {"item": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+
+
+# ******** -------- ********
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id"}
+
+
+# ******** -------- ********
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+#
+#
+# @app.get("/items")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
+
+
+# ******** -------- ********
+"""
+path parameters
+"""
 # @app.get("/files/{file_path:path}")
 # async def read_file(file_path: str):
 #     return {"file_path": file_path}
 
 
- # ******** -------- ********
+# ******** -------- ********
 
 # class ModelName(str, Enum):
 #     alexnet = "alexnet"
